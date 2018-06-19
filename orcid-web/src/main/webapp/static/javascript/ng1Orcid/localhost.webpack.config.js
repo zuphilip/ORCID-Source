aec929fb302c0cca037e2d3052079c9881d077bf
Manuel Calvo <m.calvo@ost.orcid.org>
2018-06-19 08:56:02 -0600

    testing compression and lazy load config for webpack

--- orcid-web/src/main/webapp/static/javascript/ng1Orcid/localhost.webpack.config.js
+++ orcid-web/src/main/webapp/static/javascript/ng1Orcid/localhost.webpack.config.js
@@ -11,14 +11,18 @@ module.exports = {
                 use: [
                     {
                         loader: 'ts-loader'
+                    },
+                    {
+                        loader: 'angular-router-loader'
                     }
                 ]
             }
         ]
     },
     output: {
-        path: __dirname + '/',
-        filename: "angular_orcid_generated.js"
+        chunkFilename: 'angular_orcid_generated-chunk.js',
+        filename: "angular_orcid_generated.js",
+        path: __dirname + '/'
     },
     plugins: [
         new webpack.DefinePlugin(
@@ -28,7 +32,21 @@ module.exports = {
                     'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
                 }
             }
-        )
+        ),
+
+        new webpack.optimize.CommonsChunkPlugin(
+            {
+                name: 'vendor',
+                minChunks: (module) => module.context && /node_modules/.test(module.context)
+            }
+        ),
+
+        new webpack.LoaderOptionsPlugin(
+            {
+                minimize: true,
+                debug: false
+            }
+        ),
     ],
     resolve: {
         alias: {
